66abdab5effc93e161fc9e9d7d83463933146aa5
ayahishamsalama2007-cpu <ayahishamsalama2007@gmail.com>
2025-12-18 23:34:48 -0800

    Update db.py

--- db.py
+++ db.py
@@ -17,21 +17,19 @@ def ensure_table():
     """Create table once at start-up if it does not exist."""
     with get_connection() as conn, conn.cursor() as cur:
         cur.execute("""
-            CREATE TABLE IF NOT EXISTS patients (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    gender VARCHAR(10),
-    age INT,
-    ChiefComplaint VARCHAR(100),
-    PainGrade INT,
-    BlooddpressurSystol INT,
-    BlooddpressurDiastol INT,
-    PulseRate INT,
-    RespiratoryRate INT,
-    O2Saturation INT,
-    TriageLevel TINYINT(1) NOT NULL DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+            CREATE TABLE IF NOT EXISTS patient_records (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                gender VARCHAR(10),
+                age INT,
+                paingrade INT,
+                systolic INT,
+                diastolic INT,
+                pulserate INT,
+                respiratoryrate INT,
+                o2saturation INT,
+                chiefcomplaint TEXT,
+                TriageLevel TINYINT(1) NOT NULL DEFAULT 0,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
         """)
-
         conn.commit()
-
