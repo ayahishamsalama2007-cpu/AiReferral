c05f9b4aac542a595f63e55881a53e7de81999c6
ayahishamsalama2007-cpu <ayahishamsalama2007@gmail.com>
2025-12-19 20:31:33 -0800

    Update main.py

--- main.py
+++ main.py
@@ -117,8 +117,8 @@ def summary():
 
             return jsonify(
                 total_records=len(records),
-                triage_level_0_count=count_0,
-                triage_level_1_count=count_1,
+                total_non_urgent=count_0,
+                total_urgent=count_1,
                 records=records
             )
     except Exception as e:
@@ -128,5 +128,6 @@ def summary():
 if __name__ == "__main__":
     ensure_table()
     app.run(host="0.0.0.0", port=8080, debug=False)
+
 
 
